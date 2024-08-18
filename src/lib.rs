827a561d07eaf08f2c1b5b1c5c2f5ec57bc10b16
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-08-18 16:47:28 -0400

    use async to wait for image to load before trying to display

--- src/lib.rs
+++ src/lib.rs
@@ -21,22 +21,25 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let image = web_sys::HtmlImageElement::new().unwrap();
-    let callback = Closure::once(|| {
-        web_sys::console::log_1(&JsValue::from_str("loaded"));
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            success_tx.send(());
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+
+        image.set_src("Idle (1).png");
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            5,
+        );
     });
-    image.set_onload(Some(callback.as_ref().unchecked_ref()));
-    callback.forget();
-
-    image.set_src("Idle (1).png");
-    context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        5,
-    );
 
     Ok(())
 }
