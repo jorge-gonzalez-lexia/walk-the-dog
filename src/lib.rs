1c5cf6d61a8b6c74c3b60d23e3aeed9dce919ca6
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-08-20 19:46:55 -0400

    remove call to sierpinski and load of Idle.png

--- src/lib.rs
+++ src/lib.rs
@@ -45,36 +45,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-        });
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-
-        let image = web_sys::HtmlImageElement::new().unwrap();
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
-
-        success_rx.await;
-
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
-
         let json = fetch_json("rhb.json").await.unwrap();
         // TODO: into_serde is deprecated (presumably after book was written)
         let sheet: Sheet = json
