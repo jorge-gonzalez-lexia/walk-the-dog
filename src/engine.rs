4e07bf9a33111dd272db2ae48486840df4d0e160
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-08-24 16:56:52 -0400

    ch3: track key state in engine

--- src/engine.rs
+++ src/engine.rs
@@ -17,7 +17,7 @@ pub trait Game {
 
     async fn initialize(&self) -> Result<Box<dyn Game>>;
 
-    fn update(&mut self);
+    fn update(&mut self, keystate: &KeyState);
 }
 
 const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
@@ -31,6 +31,7 @@ type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
     pub async fn start(game: impl Game + 'static) -> Result<()> {
+        let mut keyevent_receiver = prepare_input()?;
         let mut game = game.initialize().await?;
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
@@ -44,12 +45,14 @@ impl GameLoop {
         // allows borrowing the closure multiple times.
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
 
+        let mut keystate = KeyState::new();
         let g = f.clone();
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
+            process_input(&mut keystate, &mut keyevent_receiver);
             // Fixing the time step. See pp 145-151
             game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
             while game_loop.accumulated_delta > FRAME_SIZE {
-                game.update();
+                game.update(&keystate);
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
