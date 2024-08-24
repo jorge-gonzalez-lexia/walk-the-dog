e06d6b3fd3fc22d8636d7e43046baa2b44995af5
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-08-24 08:43:17 -0400

    ch3: move canvas and context to browser mode

--- src/browser.rs
+++ src/browser.rs
@@ -1,10 +1,33 @@
 use anyhow::{anyhow, Result};
-use web_sys::{Document, Window};
+use wasm_bindgen::JsCast;
+use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Window};
 
 macro_rules!log {
   ($($t:tt)*) => {
     web_sys::consol::log_1($format!($($t)*).into());
   }
+}
+
+pub fn canvas() -> Result<HtmlCanvasElement> {
+    document()?
+        .get_element_by_id("canvas")
+        .ok_or_else(|| anyhow!("No Canvas Element found with ID 'canvas'"))?
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|element| anyhow!("Error converting {:#?} to HtmlCanvasElement", element))
+}
+
+pub fn context() -> Result<CanvasRenderingContext2d> {
+    canvas()?
+        .get_context("2d")
+        .map_err(|js_value| anyhow!("Error getting 2d context {:#?}", js_value))?
+        .ok_or_else(|| anyhow!("No 2d context found"))?
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .map_err(|element| {
+            anyhow!(
+                "Error converting {:#?} to CanvasRenderingContext2d",
+                element
+            )
+        })
 }
 
 pub fn document() -> Result<Document> {
