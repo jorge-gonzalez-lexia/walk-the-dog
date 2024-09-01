c3b98d12a3e6ffd7722a153022ad84365e664e9c
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-09-01 10:12:29 -0400

    ch7: decode_audio_data

--- src/browser.rs
+++ src/browser.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use js_sys::ArrayBuffer;
 use std::future::Future;
 use wasm_bindgen::closure::{WasmClosure, WasmClosureFnOnce};
 use wasm_bindgen::prelude::*;
@@ -56,17 +57,33 @@ pub fn document() -> Result<Document> {
         .ok_or_else(|| anyhow!("No Document Found"))
 }
 
-pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
-    let resp_value = fetch_with_str(json_path).await?;
-    let resp: Response = resp_value
+pub async fn fetch_array_buffer(resource: &str) -> Result<ArrayBuffer> {
+    let array_buffer = fetch_response(resource)
+        .await?
+        .array_buffer()
+        .map_err(|err| anyhow!("Error loading array buffer {:#?}", err))?;
+    JsFuture::from(array_buffer)
+        .await
+        .map_err(|err| anyhow!("Error converting array buffer into a future {:#?}", err))?
         .dyn_into()
-        .map_err(|element| anyhow!("Error converting {:#?} to Response", element))?;
+        .map_err(|err| anyhow!("Error converting raw JSValue to ArrayBuffer {:#?}", err))
+}
+
+pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
+    let resp = fetch_response(json_path).await?;
     JsFuture::from(
         resp.json()
             .map_err(|err| anyhow!("Cold not get JSON from response {:#?}", err))?,
     )
     .await
     .map_err(|err| anyhow!("error fetching JSON {:#?}", err))
+}
+
+pub async fn fetch_response(json_path: &str) -> Result<Response> {
+    fetch_with_str(json_path)
+        .await?
+        .dyn_into()
+        .map_err(|element| anyhow!("Error converting {:#?} to Response", element))
 }
 
 pub async fn fetch_with_str(resource: &str) -> Result<JsValue> {
--- src/sound.rs
+++ src/sound.rs
@@ -1,8 +1,25 @@
 use anyhow::{anyhow, Result};
+use js_sys::ArrayBuffer;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
 use web_sys::{AudioBuffer, AudioBufferSourceNode, AudioContext, AudioDestinationNode, AudioNode};
 
 pub fn create_audio_context() -> Result<AudioContext> {
     AudioContext::new().map_err(|err| anyhow!("Could not create audio context: {:#?}", err))
+}
+
+pub async fn decode_audio_data(
+    ctx: &AudioContext,
+    array_buffer: ArrayBuffer,
+) -> Result<AudioBuffer> {
+    JsFuture::from(
+        ctx.decode_audio_data(&array_buffer)
+            .map_err(|err| anyhow!("Could not decode audio from array buffer {:#?}", err))?,
+    )
+    .await
+    .map_err(|err| anyhow!("Could not convert promise to future {:#?}", err))?
+    .dyn_into()
+    .map_err(|err| anyhow!("Could not cast into AudioBuffer {:#?}", err))
 }
 
 pub fn play_sound(ctx: &AudioContext, buffer: &AudioBuffer) -> Result<()> {
